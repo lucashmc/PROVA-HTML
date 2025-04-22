761445732c54aad4818979ab7814d131292aefaa
lucashmc <lucas.henrique_moraes@hotmail.com>
2025-04-22 20:46:52 -0300

    adiçao da tabela de valores para o usario ter noçao onde ele ta gastando e adicionei uma imagen no fundo da calculadora

--- main.js
+++ main.js
@@ -2,13 +2,32 @@ let totalGastos = 0;
 
 document.getElementById('adicionarGasto').addEventListener('click', () => {
     const valorInput = document.getElementById('valor');
+    const descricaoInput = document.getElementById('descricao');
+    const categoriaSelect = document.getElementById('categoria');
     const valor = parseFloat(valorInput.value);
+    const descricao = descricaoInput.value;
+    const categoria = categoriaSelect.value;
 
-    if (!isNaN(valor) && valor > 0) {
+    if (!isNaN(valor) && valor > 0 && descricao.trim() !== '') {
         totalGastos += valor;
         document.getElementById('totalGastos').textContent = `Total: R$ ${totalGastos.toFixed(2)}`;
-        valorInput.value = ''; // 
+
+       
+        const tabelaBody = document.getElementById('tabelaGastos').getElementsByTagName('tbody')[0];
+        const novaLinha = tabelaBody.insertRow();
+
+        const celulaCategoria = novaLinha.insertCell(0);
+        const celulaDescricao = novaLinha.insertCell(1);
+        const celulaValor = novaLinha.insertCell(2);
+
+        celulaCategoria.textContent = categoria;
+        celulaDescricao.textContent = descricao;
+        celulaValor.textContent = `R$ ${valor.toFixed(2)}`;
+
+       
+        valorInput.value = '';
+        descricaoInput.value = '';
     } else {
-        alert('Por favor, insira um valor válido.');
+        alert('Por favor, insira uma descrição e um valor válido.');
     }
 });
